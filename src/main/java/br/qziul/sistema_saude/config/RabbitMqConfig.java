52dc8252f38f7b51a20ed2aa733081d900c3ed4b
QziuL <lf.quinholi2@gmail.com>
2024-08-08 17:20:05 -0300

    feat: json convert and queue declare

--- src/main/java/br/qziul/sistema_saude/config/RabbitMqConfig.java
+++ src/main/java/br/qziul/sistema_saude/config/RabbitMqConfig.java
@@ -1,9 +1,24 @@
 package br.qziul.sistema_saude.config;
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMqConfig {
-
     public static final String TRIAGEM_QUEUE = "sistema-triagem-queue";
+
+    // Conversão de JSON para Message.
+    @Bean
+    public Jackson2JsonMessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    // Declarando a Fila.
+    @Bean
+    public Declarable triagemQueue() {
+        return new Queue(TRIAGEM_QUEUE);
+    }
 }
