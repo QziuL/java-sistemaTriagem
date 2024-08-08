bfe63c59fb7c70719262ab6d87087d03c89daff3
QziuL <lf.quinholi2@gmail.com>
2024-08-08 16:39:43 -0300

    feat: entitys Triagem, Paciente

---
+++ src/main/java/br/qziul/sistema_saude/entitys/Paciente.java
@@ -0,0 +1,79 @@
+package br.qziul.sistema_saude.entitys;
+
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+@Document(collection = "tb_pacientes")
+public class Paciente {
+    @MongoId
+    private Long pacienteId;
+
+    private String nome;
+    private String nascimento;
+
+    @Indexed(name = "cpf_index")
+    private String cpf;
+    private String telefone;
+    private String cep;
+
+    public Paciente() {}
+
+    public Long getPacienteId() {
+        return pacienteId;
+    }
+
+    public void setPacienteId(Long pacienteId) {
+        this.pacienteId = pacienteId;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNascimento() {
+        return nascimento;
+    }
+
+    public void setNascimento(String nascimento) {
+        this.nascimento = nascimento;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    @Override
+    public String toString() {
+        return "pacienteId=" + pacienteId +
+                ", nome='" + nome + '\'' +
+                ", nascimento='" + nascimento + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", cep='" + cep + '\'';
+    }
+}
