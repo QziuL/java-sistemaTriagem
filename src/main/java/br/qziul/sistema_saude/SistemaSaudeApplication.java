8c6eac9ae17e1c39fd928157cb9d9099dc6f3d05
QziuL <lf.quinholi2@gmail.com>
2024-08-08 15:08:28 -0300

    first commit

---
+++ src/main/java/br/qziul/sistema_saude/SistemaSaudeApplication.java
@@ -0,0 +1,13 @@
+package br.qziul.sistema_saude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaSaudeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaSaudeApplication.class, args);
+	}
+
+}
