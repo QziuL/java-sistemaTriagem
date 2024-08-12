4e4035260ec2d48a40bd38e2368f624972976f7f
QziuL <lf.quinholi2@gmail.com>
2024-08-12 16:15:03 -0300

    feat: endpoint update

--- src/main/java/br/qziul/sistema_saude/controller/TriagemController.java
+++ src/main/java/br/qziul/sistema_saude/controller/TriagemController.java
@@ -1,12 +1,15 @@
 package br.qziul.sistema_saude.controller;
 
 import br.qziul.sistema_saude.controller.dtos.TriagemResponse;
+import br.qziul.sistema_saude.models.entitys.TriagemEntity;
 import br.qziul.sistema_saude.service.TriagemService;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class TriagemController {
@@ -20,18 +23,34 @@ public class TriagemController {
 
     @GetMapping("/triagem/{triagemId}")
     public ResponseEntity<TriagemResponse> getById(@PathVariable("triagemId") Long triagemId) {
-        TriagemResponse triagemResponse = triagemService.findById(triagemId);
-        return ResponseEntity.ok(triagemResponse);
+        TriagemResponse triagemResponse = triagemService.findByIdAndReturnResponse(triagemId);
+        return (Objects.isNull(triagemResponse))
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(triagemResponse);
     }
 
     @DeleteMapping("/triagem/{triagemId}")
     public ResponseEntity<TriagemResponse> deleteById(@PathVariable("triagemId") Long triagemId) {
-        return ResponseEntity.ok(triagemService.deleteById(triagemId));
+        TriagemResponse triagemResponse = triagemService.deleteById(triagemId);
+        return (Objects.isNull(triagemResponse))
+                ? ResponseEntity.notFound().build()
+                : ResponseEntity.ok(triagemResponse);
     }
 
-    /*@PutMapping("/triagem/{triagemId}")
-    public ResponseEntity<TriagemResponse> updateById(@PathVariable("triagemId") Long triagemId,
-                                                      @RequestBody TriagemEntity triagemEntity) {
+    @PutMapping("/triagem/{triagemId}")
+    public ResponseEntity<Object> updateById(
+            @PathVariable("triagemId") Long triagemId,
+            @RequestBody TriagemEntity triagemEntity
+    ) {
+        TriagemEntity entity = triagemService.findByIdAndReturnEntity(triagemId);
+        if(Objects.isNull(entity))
+            return ResponseEntity.notFound().build();
 
-    }*/
+        BeanUtils.copyProperties(triagemEntity, entity);
+        System.out.println(triagemEntity.getPaciente());
+//        entity.setPaciente(triagemEntity.getPaciente());
+        triagemService.update(entity);
+
+        return ResponseEntity.ok(TriagemResponse.toResponse(entity));
+    }
 }
