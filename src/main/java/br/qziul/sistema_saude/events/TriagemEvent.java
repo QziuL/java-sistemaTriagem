8785337d93acc7cfec758b8ce663ee90161d96ca
QziuL <lf.quinholi2@gmail.com>
2024-08-12 16:22:03 -0300

    style: removed comment

--- src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
+++ src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
@@ -3,9 +3,6 @@ package br.qziul.sistema_saude.events;
 import br.qziul.sistema_saude.models.Paciente;
 import br.qziul.sistema_saude.models.entitys.TriagemEntity;
 
-/**
- *  Verificar como alterar DTO para não receber os IDs.
- */
 public record TriagemEvent(
         Long codigoTriagem,
         PacienteEvent paciente,
@@ -21,7 +18,6 @@ public record TriagemEvent(
 
     private static Paciente setPaciente(TriagemEvent triagemEvent) {
         return new Paciente(
-                triagemEvent.paciente().codigoPaciente(),
                 triagemEvent.paciente().nome(),
                 triagemEvent.paciente().nascimento(),
                 triagemEvent.paciente().cpf(),
