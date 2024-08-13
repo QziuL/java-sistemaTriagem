4218d3008dcdf6ad98415ff8faf36d1a7bc50c55
QziuL <lf.quinholi2@gmail.com>
2024-08-12 21:02:44 -0300

    refactor: ID's name

--- src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
+++ src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
@@ -4,13 +4,13 @@ import br.qziul.sistema_saude.models.Paciente;
 import br.qziul.sistema_saude.models.entitys.TriagemEntity;
 
 public record TriagemEvent(
-        Long codigoTriagem,
+        Long triagemId,
         PacienteEvent paciente,
         boolean atendido
 ) {
     public TriagemEntity toEntity() {
         TriagemEntity triagemEntity = new TriagemEntity();
-        triagemEntity.setTriagemId(this.codigoTriagem());
+        triagemEntity.setTriagemId(this.triagemId());
         triagemEntity.setAtendido(this.atendido());
         triagemEntity.setPaciente(setPaciente(this));
         return triagemEntity;
