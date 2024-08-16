dd9ab96257e7bea50a5419b65be2bf451715e8ac
QziuL <lf.quinholi2@gmail.com>
2024-08-16 17:13:10 -0300

    refactor: does not receive ID from TriagemEvent

--- src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
+++ src/main/java/br/qziul/sistema_saude/events/TriagemEvent.java
@@ -4,13 +4,11 @@ import br.qziul.sistema_saude.models.Paciente;
 import br.qziul.sistema_saude.models.entitys.TriagemEntity;
 
 public record TriagemEvent(
-        Long triagemId,
         PacienteEvent paciente,
         boolean atendido
 ) {
     public TriagemEntity toEntity() {
         TriagemEntity triagemEntity = new TriagemEntity();
-        triagemEntity.setTriagemId(this.triagemId());
         triagemEntity.setAtendido(this.atendido());
         triagemEntity.setPaciente(setPaciente(this));
         return triagemEntity;
